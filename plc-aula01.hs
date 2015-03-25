6a6740f4b13c77f97a0082de103edda5c2df97df
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-03-25 12:59:06 -0300

    adicionado mais algumas funcoes

--- plc-aula01.hs
+++ plc-aula01.hs
@@ -28,5 +28,16 @@
  	| n == 0 = vendas 0
  	| otherwise = totalVendas (n-1) + vendas n
 
+ --somear elementos de uma lista de forma recursiva
+ sumList :: [Int] -> Int
+ sumList as 
+ 	| as == [] = 0
+ 	| otherwise = (head as) + sumList (tail as)
+
+--dobrar os elementos de uma lista
+doubleList :: [Int] -> [Int]
+doubleList [] = []
+doubleList (n:ns) = (2 * n) : doubleList ns
+
 
 
