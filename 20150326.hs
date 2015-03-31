0f3599c3d038e12529b5dba2fd12764ba331c501
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-03-30 21:38:02 -0300

    implementações finais

--- 20150326.hs
+++ 20150326.hs
@@ -24,3 +24,32 @@ hasKey :: HashTable -> Integer -> Bool
 hasKey as hashKey
 	| get as hashKey == "Nops" = False
 	| otherwise = True
+
+member :: (Eq a) => a -> [a] -> Bool
+member _ [] = False
+member n (x:xs)
+	| n == x = True
+	| otherwise = member n xs
+
+union :: (Eq a) => [a] -> [a] -> [a]
+union xs [] = xs
+union [] ys = ys
+union (x:xs) ys
+	| (member x ys) = union xs ys
+	| otherwise = union xs $ [x] ++ ys 
+
+intersection :: (Eq a) => [a] -> [a] -> [a]
+intersection xs ys = [element | element <- union xs ys, member element xs, member element ys]
+
+difference :: (Eq a) => [a] -> [a] -> [a]
+difference xs ys = [element | element <- (intersection xs xs), not $ member element ys]
+
+comparaConjuntos :: (Eq a) => [a] -> [a] -> String
+comparaConjuntos set1 set2 
+	| ((intersection set1 set2) == []) = "Conjutos Disjuntos"
+	| (set1 == set2) = "A igual a B"
+	| (not ((intersection set1 set2) == set1)), (not ((intersection set1 set2)== set2)) = "A intersiona B"
+	| ((difference set1 set2) == []) = "B contem A"
+	| ((difference set2 set1) == []) = "A contem B"
+
+--comparaConjuntos :: (Eq t) => [t] -> [t] -> String
