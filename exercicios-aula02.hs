ff0d1cf4165f8025a0673cc9439989f2871af8cf
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-04-13 21:05:46 -0300

    update exercícios aula 02

--- exercicios-aula02.hs
+++ exercicios-aula02.hs
@@ -79,7 +79,66 @@ pontoY x ((x1, y1),(x2, y2))
 	| otherwise = ((y2 - y1)/(x2 - x1)) * (x - x1) + y1
 
 
+-- expressão case
+firstDigit :: String -> Char
+firstDigit st = case (digits st) of
+	[] -> '\0'
+	(a:as) -> a
 
+-- compreensão de listas
+
+doubleList :: [Int] -> [Int]
+doubleList xs = [2*a | a <- xs, isEven a] 
+
+isEven :: Int -> Bool
+isEven a
+	| a `mod` 2 == 0 = False
+	| otherwise = True
+
+sumPairs :: [(Int, Int)] -> [Int]
+sumPairs lp = [a+b | (a,b) <- lp]
+
+isDigit :: Char -> Bool
+isDigit x
+	| x == '0' || x == '1' || x == '2' || x == '3' || x == '4' || x == '5' || x == '6' || x == '7' || x == '8' || x == '9' = True
+	| otherwise = False
+
+digits :: String -> String
+digits st = [a | a <- st, isDigit a]
+
+myLength :: [t] -> Int
+myLength [] = 0
+myLength (a:as) = 1 + myLength as
+
+-- exemplo biblioteca
+type Pessoa = String
+type Livro = String
+type BancoDados = [(Pessoa, Livro)]
+
+baseExemplo :: BancoDados
+baseExemplo = 
+	[("Sergio", "O Senhor dos Aneis"),
+	("Andre", "Duna"),
+	("Fernando", "Jonathan Strange & Mr. Norrell"),
+	("Fernando", "A Game of Thrones")]
+
+livros :: BancoDados -> Pessoa -> [Livro]
+livros [] _ = []
+livros (x:xs) p
+	| fst x == p = (snd x):livros xs p
+	| otherwise = livros xs p
+
+emprestimos :: BancoDados -> Livro -> [Pessoa]
+emprestimos [] _ = []
+emprestimos (x:xs) l
+	| snd x == l = fst x : emprestimos xs l
+	| otherwise = emprestimos xs l
+
+emprestado :: BancoDados -> Livro -> Bool
+emprestado [] _ = False
+emprestado (x:xs) l
+	| snd x == l = True
+	| otherwise = emprestado xs l
 
 
 
