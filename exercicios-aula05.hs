4f3b3ee6d9597644346a0fe69ade18c9e2184201
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-05-06 11:33:36 -0300

    add mais funções

--- exercicios-aula05.hs
+++ exercicios-aula05.hs
@@ -108,6 +108,73 @@ myMember el (x:xs)
 	| (myFoldr (-) el [x]) == 0 = True
 	| otherwise = myMember el xs
 
+anotherMember :: Eq t => t -> [t] -> Bool
+anotherMember f ls = foldr (||) False (map (==f) ls) 
+
+anotherUnion :: [t] -> [t] -> [t]
+anotherUnion a b = foldr (:) [] a ++ foldr (:) [] b
+
+somaOrdString :: [String] -> [Int]
+somaOrdString [] = []
+somaOrdString (x:xs) = sumList (posicaoAlfabeto x) : somaOrdString xs
+
+--criarArvore :: Ord(t) => [t] -> (Tree t -> t -> Tree t) -> Tree t
+
+myFilter :: (t -> Bool) -> [t] -> [t]
+myFilter _ [] = []
+myFilter p (a:as)
+	| p a = a : myFilter p as
+	| otherwise = myFilter p as
+
+myEvens :: [Int] -> [Int]
+myEvens xs = myFilter isEven xs
+	where isEven n = (n `mod` 2 == 0)
+
+filterListComprehention :: (t -> Bool) -> [t] -> [t]
+filterListComprehention p l = [a | a <- l, p a]
+
+filterListArgument :: [[t]] -> (t -> Bool) -> [[t]]
+filterListArgument [] f = []
+filterListArgument (x:xs) f = [a | a <- x, f a] : filterListArgument xs f
+
+
+filterFoldrListArgument :: Int -> [[Int]] -> [[Int]]
+filterFoldrListArgument n [] = []
+filterFoldrListArgument n (x:xs) = [a | a <- x, (foldr (+) 0 x) < n] : filterFoldrListArgument n xs
+
+inter :: [Int] -> [Int] -> [Int]
+inter _ [] = []
+inter [] _ = []
+inter (a:as) l = filter (== a) l ++ inter as l 
+
+diff :: [Int] -> [Int] -> [Int]
+diff _ [] = []
+diff [] _ = []
+diff (a:as) (b:bs)
+	| myMember b (a:as) = diff (a:as) bs
+	| myMember a (b:bs) = diff (b:bs) as
+	| otherwise = b : diff (a:as) bs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
