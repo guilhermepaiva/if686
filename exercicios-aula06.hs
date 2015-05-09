2949cc754524c536d4323031caeb6c844a5d4ce6
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-05-09 09:46:44 -0300

    update

--- exercicios-aula06.hs
+++ exercicios-aula06.hs
@@ -4,4 +4,9 @@ multiply :: Int -> Int -> Int
 multiply a b = a*b
 
 doubleList :: [Int] -> [Int]
-doubleList = map (multiply 2) 
+doubleList = map (multiply 2)
+
+add :: Int -> (Int -> Int)
+add x y = x + y
+
+addOne = add + 1
