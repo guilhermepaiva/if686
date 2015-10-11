0f4f67e7e55c18a0e380ca1c6aebc01cfec7f4b6
guilhermepaiva <guilhermepaiva9@gmail.com>
2015-10-10 21:36:52 -0300

    início da aula TiposAbstratosPolimorfismoParametricoLimitado

--- TiposAbstratosPolimorfismoParametricoLimitado.hs
+++ TiposAbstratosPolimorfismoParametricoLimitado.hs
@@ -1,20 +1,46 @@
-data OpBinario = Soma | Sub | Mult | Div
-data OpUnario = Fatorial | Menos
--- data Exp = Literal Float | Unaria OpUnario Exp | Binaria Exp OpBinario Exp ====>>> definição antiga
-data Exp t = Literal t | Unaria OpUnario (Exp t) | Binaria (Exp t) OpBinario (Exp t) -- o tipo de Exp passado como parâmetro
+type Operador = Char
+data Expressao = Numero Float | Unaria Operador Expressao | Binaria Expressao Operador Expressao
+
+avOp :: Fractional a => Char -> a -> a -> a
+avOp '+' = (+)
+avOp '-' = (-)
+avOp '*' = (*)
+avOp '/' = (/)
+
+avaliard :: Expressao -> Float
+avaliard (Numero f) = f
+avaliard (Binaria e1 op e2) = avOp op (avaliard e1) (avaliard e2)
+avaliard (Unaria op e)
+	| op == '-' = -(avaliard e)
+	| op == '+' = avaliard e
+
+-- testes
+exp1 = (Numero 3)
+exp2 = (Binaria (Numero 3) '+' (Numero 2))
+exp3 = (Unaria '-' (Numero 3))
+exp4 = (Unaria '+' (Numero 9))
+exp5 = Binaria (Numero 4) '+' (Binaria (Numero 6) '/' (Numero 2))
+
+resexp1 = avaliard exp1
+resexp2 = avaliard exp2
+resexp3 = avaliard exp3
+resexp4 = avaliard exp4
+resexp5 = avaliard exp5
 
 
-av Soma = (+)
-av Sub = (-)
-av Mult = (*)
-av Div = (/)
 
+showExpressao (Numero f) = show f
+showExpressao (Binaria e1 op e2) = "(" ++ (showExpressao e1) ++ (showOp op) ++ (showExpressao e2) ++ ")"
+showExpressao (Unaria op e) = (showOp op) ++ (showExpressao e)
 
-avaliar :: Fractional a => Exp (t -> a) -> a
-avaliar a (Literal n) = a n
-avaliar a (Binaria e op f) = (av op) (avaliar a e) (avaliar a f)
+showOp '+' = " + "
+showOp '-' = " - "
+showOp '*' = " * "
+showOp '/' = " / "
 
---exemplos
-{--
-ex1 = avaliar (Binaria (Literal 3) Soma (Literal 2))
-ex2 = avaliar (Literal 3)--}
+-- testes
+showExp1 = showExpressao exp1
+showExp2 = showExpressao exp2
+showExp3 = showExpressao exp3
+showExp4 = showExpressao exp4
+showExp5 = showExpressao exp5
